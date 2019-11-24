9f52f573dcd100c18a32437982ac8488c01ba1e6
Kim T <kimturley@gmail.com>
2019-11-23 16:51:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# webpack-babel-es6
+
+Example modern JavaScript project using:
+
+* Webpack 4.41.x
+* Babel 7.7.x
+* Scss 4.13.x
+
+
+## Installation
+
+Install dependencies using:
+
+    npm install
+
+
+## Usage
+
+Run the dev server using:
+
+    npm start
+
+Then view the local server at:
+
+    http://localhost:8080
+
+
+## Directory structure
+
+    /                                 --> Project root
+    /src                              --> Frontend sources files
+
+
+## Contact
+
+For more information please contact kmturley
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const OUTPUT_FOLDER = 'dist';
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, OUTPUT_FOLDER),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: './src/index.html' },
+    ]),
+  ],
+  devServer: {
+    contentBase: './' + OUTPUT_FOLDER,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+};
